8e8047631f14ab1c42b1b30cbfb13d8bea748a05
apoorvaa <apoorvaagowda08@gmail.com>
2025-01-29 11:09:38 +0530

    Remove all LFS files from history

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // React plugin
-import svgr from 'vite-plugin-svgr';      // SVG-to-React component plugin
-
-export default defineConfig({
-  plugins: [react(), svgr()]  // Use both plugins
-});
-
